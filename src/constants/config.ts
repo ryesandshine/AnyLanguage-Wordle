024ad4e243ee597d154b2e957312ce469098722d
ryesandshine <sherrycpanganiban@gmail.com>
2024-06-10 15:46:54 +0800

    translation array

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  tries: 7, // This changes how many tries you get to finish the game
+  tries: 6, // This changes how many tries you get to finish the game
   language: 'Filipino', // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
   author: 'La Pinay', // Put your name here so people know who made this game!
@@ -14,6 +14,6 @@ export const CONFIG = {
   normalization: 'NFC', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
   startDate: 'July 01, 2024 00:00:00', // what date and time to start your game from
   defaultLang: 'en', // the default interface language
-  availableLangs: ['en'], // the options available to the user for translation languages
+  availableLangs: 'en', // the options available to the user for translation languages
   escapeSpecialCharacters: true, // whether to escape all characters in the orthography.
 }
